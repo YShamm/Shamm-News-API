d398a528c171338ec0ca12f932d4dbd69b074846
Admin <macbookair2021@Macbooks-MacBook-Air.local>
2023-10-03 14:56:34 +0100

    implement get articlesById

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -54,3 +54,52 @@ describe("GET /API", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id, gets an article by its id", () => {
+  test("status 200; returns article w/ correct id and required properties", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then((response) => {
+        console.log(response, "response in test");
+        const article = response.body.article;
+        console.log(article, "log of article in test");
+        expect(article).toHaveProperty("title", expect.any(String)); //note: the property needs to be in ""
+        expect(article).toHaveProperty("topic", expect.any(String));
+        expect(article).toHaveProperty("author", expect.any(String));
+        expect(article).toHaveProperty("body", expect.any(String));
+        expect(article).toHaveProperty("created_at", expect.any(String));
+        expect(article).toHaveProperty("votes", expect.any(Number));
+        expect(article).toHaveProperty("article_img_url", expect.any(String));
+        expect(article).toHaveProperty("article_id", 3);
+
+        expect(article).toEqual({
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "some gifs",
+          created_at: "2020-11-03T09:12:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("status 404; id not found - id type correct but does not exist", () => {
+    return request(app)
+      .get("/api/articles/9000")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("article id not found");
+      });
+  });
+  test("status 400; invalid id type - id type incorrect", () => {
+    return request(app)
+      .get("/api/articles/three")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("invalid id");
+      });
+  });
+});
--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,4 +1,4 @@
-const { fetchTopics } = require("../models/api.models");
+const { fetchTopics, fetchArtileById } = require("../models/api.models");
 const endpoints = require("../endpoints.json");
 
 exports.getTopics = (request, response, next) => {
@@ -14,4 +14,17 @@ exports.getTopics = (request, response, next) => {
 
 exports.getAPI = (request, response, next) => {
   response.status(200).send(endpoints);
+};
+
+exports.getArticleById = (request, response, next) => {
+  //   console.log(request, "request in controller");
+  const id = request.params.article_id;
+  fetchArtileById(id)
+    .then((fetchedArticle) => {
+      console.log(fetchedArticle, "fArticle in controller");
+      response.status(200).send({ article: fetchedArticle });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
